bd3ab15a9983d9cbf2df860c54de9965c34a4a28
nattaphat010447 <jangkup90@gmail.com>
2025-09-13 01:10:10 +0700

    Update Sprint 2 in main.py

--- main.py
+++ main.py
@@ -10,6 +10,35 @@ def load_books():
             return json.load(file)
     return []
 
+#บันทึกหนังสือ
+def save(books):
+    with open(file_name,"w",encoding="utf-8") as file:
+        json.dump(books, file, ensure_ascii=False, indent=4)
+
+#เพิ่มฟังก์ชั่น เพิ่มหนังสือ
+def add_book(books):
+    title = ""
+    author = ""
+    while not title:
+        title = input("กรุณาใส่ชื่อหนังสือ : ").strip()
+        if not title:
+            print("ไม่มีชื่อหนังสือ กรุณาใส่ชื่อหนังสือ")
+
+
+    while not author:
+        author = input("กรุณาใส่ชื่อผู้เขียน : ").strip()
+        if not author:
+            print("ไม่มีชื่อหนังสือ กรุณาใส่ชื่อผู้เขียน")
+
+    for book in books:
+        if book['title'] == title and book['author'] == author:
+            print("\n!!หนังสือเล่มนี้มีอยู่แล้วในระบบ!!\n")
+            return
+
+    books.append({"title" : title, "author" : author})
+    save(books)
+    print(f"\n!!เพิ่มหนังสือเสร็จสิ้น ({title})!!\n")
+
 def main():
     books = load_books()
     print("Welcome to Book Registration!\n")
@@ -19,7 +48,7 @@ def main():
         choice = input("กรุณาใส่ตัวเลือก : ")
 
         if choice == "1":
-            print("TODO: add_book")
+           add_book(books)
         elif choice == "2":
             print("TODO: show_book")
         elif choice == "3":
@@ -29,12 +58,12 @@ def main():
         elif choice == "5":
             print("TODO: edit_book")
         elif choice == "6":
-            print(f"\nจัดทำโดย\n\n1.นายณัฐพัฒน์ แสนตรี 663380012-6\n2.นางสาวการติมา คำภีร์ 663380199-4")
+            print(f"\nจัดทำโดย\n\n1.นายณัฐพัฒน์ แสนตรี 663380012-6 (Coder)\n2.นางสาวการติมา คำภีร์ 663380199-4 (Planner)")
         elif choice == "0":
             print("Goodbye!")
             break
         else:
-            print("\n Invalid command. Please try again.\n")
+            print("\nตัวเลือกผิดพลาด กรุณาใส่ตัวเลือกที่มีอยู่\n")
 
 if __name__ == "__main__":
     main()
